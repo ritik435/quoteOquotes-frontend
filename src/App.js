0b4838defa82c1c0c60384c2baca0718deddc753
Nurul Mukhlisa <numulaa@gmail.com>
2023-04-16 15:36:03 +0800

    added home page UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Profile from "./pages/Profile/Profile";
+import Contact from "./pages/ContactUs/Contact";
+import Login from "./pages/Login/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Lets Start with the QuoteOQuotes</h1>
-        <p>Nurul made changes</p>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
