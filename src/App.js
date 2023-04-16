db6d10c8997146c15482ed447f26f8c0b246385c
Nurul Mukhlisa <numulaa@gmail.com>
2023-04-16 15:30:44 +0800

    added text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Lets Start with the QuoteOQuotes</h1>
+        <p>Nurul made changes</p>
       </header>
     </div>
   );
