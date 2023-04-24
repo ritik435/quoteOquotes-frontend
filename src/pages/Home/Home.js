4b6bae1aa441ff223ee49f6836ea4067e07e2ece
Nurul Mukhlisa <numulaa@gmail.com>
2023-04-24 16:52:20 +0800

    fetched quotes from backend api

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./Home.css";
 import QuoteCard from "../../components/UI/QuoteCard";
 
 const Home = () => {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    fetchRandomQuote();
+  }, []);
+  const fetchRandomQuote = async () => {
+    try {
+      const response = await axios.get(
+        "https://quote-o-quotes-backend.vercel.app/get-posts"
+      );
+      const randomIndex = Math.floor(
+        Math.random() * response.data.result.length
+      );
+      const randomQuote = response.data.result[randomIndex];
+      setQuote(randomQuote);
+    } catch (error) {
+      console.error("Error fetching random quote:", error);
+    }
+  };
   return (
     <main>
-      <h1>THOUGHT OF THE DAY</h1>
+      <h1>Discover, Create, Share!</h1>
       <div className="hero">
         <section className="quote-sec">
-          <QuoteCard />
-          <button className="shuffle-btn">Shuffle</button>
+          <QuoteCard quote={quote} />
+          <button className="shuffle-btn" onClick={fetchRandomQuote}>
+            Shuffle
+          </button>
         </section>
         <section className="hero-text">
-          <h3>Affirmations to make your day positive and happy</h3>
+          <h3>
+            Unleash the Power of Words with Our Quote App: Discover, Create, and
+            Share Inspirational Quotes!
+          </h3>
           <p>
-            I am the strongest person in this universe, no one can take over me.
-            I have a peaceful mind which is helping me to create a strong impact
-            on this world. I am the happiest person just because I don't lie to
-            anyone, including myself. So I will just be truthful, always. Thank
-            you :)
+            Introducing our powerful and user-friendly quote app! Discover a
+            world of inspiration at your fingertips with our vast collection of
+            quotes from famous authors, philosophers, and thought leaders.
+            Easily add your own favorite quotes and customize your personal
+            collection. Share meaningful quotes with friends, family, and social
+            media followers to spread positivity and wisdom. Whether you're
+            seeking motivation, reflection, or just a dose of daily inspiration,
+            our quote app is your go-to resource. Download now and let the magic
+            of words inspire your soul!
           </p>
         </section>
       </div>
